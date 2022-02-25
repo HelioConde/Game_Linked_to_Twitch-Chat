02f5c194bc4de9de9315fa7cf09b74f21085ab3b
Hélio Conde <68510529+HelioConde@users.noreply.github.com>
2022-02-25 18:49:20 -0300

    update imgs

--- js/script.js
+++ js/script.js
@@ -17,9 +17,8 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
     // Criar Personagem //
     if (extra.customRewardId == "5b9999e9-23c0-4ee4-b257-1ae87d1d5120") {
         console.log("Create Char?")
-        if (message <= 4)
-            playerChar = message;
-        else if (message >= 1)
+
+        if (message > 0 && message < 21)
             playerChar = message;
         else
             playerChar = 1;
@@ -110,15 +109,28 @@ function loadGame() {
     playerName = JSON.parse(localStorage.getItem('players'));
 
     let left = 0;
+    let bottom;
+    let index;
+
     for (obj in playerName) {
-        let bottom = parseInt(Math.random() * (75 - 1));
-        players.innerHTML = players.innerHTML + `<div class="playerChar" style="left:` + left + `px;bottom:` + bottom + `px;" alt="' + obj + '">
-        <div class="playerName">` + playerName[obj].player.nome + `</div>
-        <div class="playerLevel">Level ` + playerName[obj].player.level + `</div>
-        <div class="char` + playerName[obj].player.char + `"></div>
-        <div class="sword1"></div>
+        var n = obj;
+        var total = n;
+        var resultado = total % 2 == 0 ? 'Par' : 'Impar';
+        if (resultado == 'Par') {
+            bottom = Math.floor(Math.random() * (70 - 55)) + 55;
+            index = 3;
+        } else {
+            bottom = Math.floor(Math.random() * (25 - 10)) + 10;
+            index = 4;
+        }
+        players.innerHTML = players.innerHTML + `
+        <div class="playerChar" style="z-index:` + index + `;left:` + left + `px;bottom:` + bottom + `px;" alt="' + obj + '">
+            <div class="playerName">` + playerName[obj].player.nome + `</div>
+            <div class="playerLevel">Level ` + playerName[obj].player.level + `</div>
+            <div class="char` + playerName[obj].player.char + `"></div>
+            <div class="sword1"></div>
         </div>`;
-        left = left + 150;
+        left = left + 75;
     }
 }
 loadGame();
